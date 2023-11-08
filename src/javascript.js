a392e3e3bf94ae9c214e9405b5cc0b0e825a2d99
dgthaina <thainadagostini61@gmail.com>
2023-11-08 10:13:14 -0300

    atualização media iphone

--- src/javascript.js
+++ src/javascript.js
@@ -1,16 +1,16 @@
-window.onscroll = function() {myFunction()};
+window.onscroll = function () { myFunction() };
 
 function myFunction() {
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
-  document.getElementById("barra").style.width = scrolled + "%";
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("barra").style.width = scrolled + "%";
 };
 
 
 // navbar colorida
 window.addEventListener("scroll", () => {
-  var navbar2 = document.getElementById("navbar");
+    var navbar2 = document.getElementById("navbar");
     if (window.scrollY > 0) {
         navbar2.style.backgroundColor = "#7be0ff";
     } else {
